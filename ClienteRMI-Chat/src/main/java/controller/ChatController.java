02c01d5f161a059e8bef8fd0727668e631456cfa
a24francisco <a24franciscoaa@iesantonlosada.gal>
2026-02-16 20:38:13 +0100

    InterfazBasica

---
+++ ClienteRMI-Chat/src/main/java/controller/ChatController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import alende.clientermi.chat.Views.ChatView;
+
+/**
+ *
+ * @author frana
+ */
+public class ChatController {
+    ChatView view;
+
+    public ChatController(ChatView view) {
+        this.view = view;
+    }
+    
+    
+}
---
+++ ClienteRMI-Chat/src/main/java/controller/RegController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import alende.clientermi.chat.Views.MainView;
+
+/**
+ *
+ * @author frana
+ */
+public class RegController {
+    public MainView view;
+
+    public RegController(MainView view) {
+        this.view = view;
+    }
+    
+    
+}
